92f4a8b8e790293422bd7e15f49741661e4d38f3
MichelBrunGit <michelmbrun@gmail.com>
2021-12-10 19:16:03 +0100

    update project DB+Results

--- MySQL/Project2_SQL.sql
+++ MySQL/Project2_SQL.sql
@@ -21,14 +21,6 @@ from health_female
 join survival_female on health_female.year=survival_female.year
 join school_female on health_female.year=school_female.year;
 
-create table hci_female
-as
-SELECT health_female.year as year, 
-(((health_female.health_female-78.0440362630324)/3.298332790895799)+((survival_female.survival_female-36.6)/16.9)+((school_female.school_female-87.62318)/0.7131399999999957))/3 as hci_female
-from health_female 
-join survival_female on health_female.year=survival_female.year
-join school_female on health_female.year=school_female.year;
-
 SELECT 
     MIN(health_female.health_female) AS min,
     (MAX(health_female.health_female) - MIN(health_female.health_female)) AS rang
@@ -62,14 +54,6 @@ from health_male
 join survival_male on health_male.year=survival_male.year
 join school_male on health_male.year=school_male.year;
 
-create table hci_male
-as
-SELECT health_male.year as year, 
-(((health_male.health_male-69.919576079512)/3.751534428233498)+((survival_male.survival_male-40.9)/16.4)+((school_male.school_male-89.20291)/1.3286599999999993))/3 as hci_male
-from health_male 
-join survival_male on health_male.year=survival_male.year
-join school_male on health_male.year=school_male.year;
-
 SELECT 
     MIN(health_male.health_male) AS min,
     (MAX(health_male.health_male) - MIN(health_male.health_male)) AS rang
@@ -90,31 +74,3 @@ SELECT
 FROM
     school_male;
 -- 89.20291	1.3286599999999993
-
-create table male
-as
-SELECT health_male.year as year, 
-health_male,
-survival_male,
-school_male,
-hci_male,
-employment_male
-from health_male 
-join survival_male on health_male.year=survival_male.year
-join school_male on health_male.year=school_male.year
-join employment_male on health_male.year=employment_male.year
-join hci_male on hci_male.year=school_male.year;
-
-create table female
-as
-SELECT health_female.year as year, 
-health_female,
-survival_female,
-school_female,
-hci_female,
-employment_female
-from health_female 
-join survival_female on health_female.year=survival_female.year
-join school_female on health_female.year=school_female.year
-join employment_female on health_female.year=employment_female.year
-join hci_female on hci_female.year=school_female.year;
